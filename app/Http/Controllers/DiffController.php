fba47f824331a0ae15506690d10e415323aa8506
Thomas Fini Hansen <xen@xen.dk>
2019-06-01 23:45:15 +0200

    Always return success

--- app/Http/Controllers/DiffController.php
+++ app/Http/Controllers/DiffController.php
@@ -4,6 +4,7 @@ namespace Appocular\Differ\Http\Controllers;
 
 use Appocular\Differ\Jobs\DiffRequest;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
 class DiffController extends Controller
 {
@@ -18,5 +19,7 @@ class DiffController extends Controller
         ]);
 
         dispatch(new DiffRequest($request->input('image_kid'), $request->input('baseline_kid')));
+        // Always return success.
+        return new Response();
     }
 }
