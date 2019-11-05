3e9e88224ebb5489da75bbc0a2da4b70ecee8d81
Thomas Fini Hansen <xen@xen.dk>
2019-11-05 22:13:50 +0100

    Increase keeper timeout

--- config/keeper.php
+++ config/keeper.php
@@ -3,5 +3,5 @@
 return [
     'base_uri' => env('KEEPER_BASE_URI', ''),
     'shared_token' => env('SHARED_TOKEN', ''),
-    'timeout' => env('KEEPER_TIMEOUT', 5),
+    'timeout' => env('KEEPER_TIMEOUT', 30),
 ];
